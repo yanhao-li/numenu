152ab37bcd0ed16f1a455b37e303a4b312621cc1
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2021-03-15 09:24:59 -0400

    removed monkeypatch

--- spec/controllers/dish_reviews_controller_spec.rb
+++ spec/controllers/dish_reviews_controller_spec.rb
@@ -1,20 +1,5 @@
 require 'spec_helper'
 
-if RUBY_VERSION>='2.6.0'
-  if Rails.version < '5'
-    class ActionController::TestResponse < ActionDispatch::TestResponse
-      def recycle!
-        # hack to avoid MonitorMixin double-initialize error:
-        @mon_mutex_owner_object_id = nil
-        @mon_mutex = nil
-        initialize
-      end
-    end
-  else
-    puts "Monkeypatch for ActionController::TestResponse no longer needed"
-  end
-end
-
 
 describe DishReviewsController do
   

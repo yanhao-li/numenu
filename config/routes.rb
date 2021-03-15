c67a66fbf36c9e585d1d8def26f7a12c6961feee
Matthew Otto <mattmotto18@gmail.com>
2021-03-14 19:29:16 -0500

    super basic functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/restaurants", to: "restaurants#index"
+  get "/restaurant_review", to: "restaurant_reviews#index"
 end

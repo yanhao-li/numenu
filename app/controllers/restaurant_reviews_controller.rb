7d92871e4daf82d05c2d4b8e8d371dbc6098bc59
Matthew Otto <mattmotto18@gmail.com>
2021-03-14 23:24:08 -0500

    completed basic functionality for submitting and viewing reviews

--- app/controllers/restaurant_reviews_controller.rb
+++ app/controllers/restaurant_reviews_controller.rb
@@ -1,15 +1,38 @@
 class RestaurantReviewsController < ApplicationController
 	def index
 		id = params[:format]
+		puts id
+		puts "yay"
 		@restaurant = nil
 		@reviews = nil
 		if id
 			@restaurant = Restaurant.find(id)
 			@reviews = @restaurant.restaurantReviews
 		end
-
+		puts @reviews.size
+		puts "here"
 	end
 
 	def new
+		id = params[:format]
+		@restaurant = nil
+		if id
+			@restaurant = Restaurant.find(id)
+		end
+	end
+
+	def create
+		puts ""
+		puts params
+		puts ""
+		restaurant_id = params[:format]
+		inputs = params[:restaurant_review]
+		review = inputs[:review]
+		rating = inputs[:rating]
+	    @review = RestaurantReview.create!(review: review, rating: rating, restaurant_id: restaurant_id)
+	    flash[:notice] = "Review was successfully submitted."
+	    puts @review
+	    puts "done"
+	    redirect_to restaurants_path
 	end
 end

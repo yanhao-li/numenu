4ad82b15cd9d244d8c5e81dd18b8e0dfdab7449c
Yanhao Li <hi@yanhaoli.com>
2021-03-15 16:09:28 -0400

    refactor: restaurants and dishes url

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/restaurants", to: "restaurants#index"
+  resources :restaurants
   get "/restaurant_reviews", to: "restaurant_reviews#index"
   get "/restaurant_reviews/new", to: "restaurant_reviews#new"
   post "/restaurant_reviews", to: "restaurant_reviews#create"
   get "/home", to: "home#index"
-  get "/dishes", to: "dishes#index"
   get "/dish_reviews", to: "dish_reviews#index"
   get "dish_reviews/new", to: "dish_reviews#new"
   post "/dish_reviews", to: "dish_reviews#create"

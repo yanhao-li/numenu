ccf5ab97996b3b1103e7b6a08e667dc7a556f214
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2021-03-15 11:12:46 -0400

    added coverage stats

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
   RSpec.describe HomeController, type: :controller do
-    pending
+    describe 'index' do
+    	it 'should render the home page' do
+      		get :index
+      		expect(response).to render_template('index')
+    	end
+  	end
   end

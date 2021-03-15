7c847b8a08be20d924a428fec3a565d29afe1c8f
Matthew Otto <mattmotto18@gmail.com>
2021-03-14 23:26:39 -0500

    removed code used for testing

--- app/controllers/restaurant_reviews_controller.rb
+++ app/controllers/restaurant_reviews_controller.rb
@@ -1,16 +1,12 @@
 class RestaurantReviewsController < ApplicationController
 	def index
 		id = params[:format]
-		puts id
-		puts "yay"
 		@restaurant = nil
 		@reviews = nil
 		if id
 			@restaurant = Restaurant.find(id)
 			@reviews = @restaurant.restaurantReviews
 		end
-		puts @reviews.size
-		puts "here"
 	end
 
 	def new
@@ -22,17 +18,12 @@ class RestaurantReviewsController < ApplicationController
 	end
 
 	def create
-		puts ""
-		puts params
-		puts ""
 		restaurant_id = params[:format]
 		inputs = params[:restaurant_review]
 		review = inputs[:review]
 		rating = inputs[:rating]
 	    @review = RestaurantReview.create!(review: review, rating: rating, restaurant_id: restaurant_id)
 	    flash[:notice] = "Review was successfully submitted."
-	    puts @review
-	    puts "done"
 	    redirect_to restaurants_path
 	end
 end

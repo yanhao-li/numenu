6611d9c7b7c655cf7f38ec9f5afabcc0622d945b
Yanhao Li <hi@yanhaoli.com>
2021-03-15 19:51:06 -0400

    fix: url on new dish reviews page

--- app/controllers/dish_reviews_controller.rb
+++ app/controllers/dish_reviews_controller.rb
@@ -5,17 +5,15 @@ class DishReviewsController < ApplicationController
 
 	def new
 		@dish = Dish.find(params[:dish_id])
-		@restaurant = Restaurant.find(@dish.id)
 	end
 
 	def create
-		restaurant_id = params[:restaurant]
-		dish_id = params[:dish]
+		dish_id = params[:dish_id]
 		inputs = params[:dish_review]
 		review = inputs[:review]
 		rating = inputs[:rating]
 	    @review = DishReview.create!(review: review, rating: rating, dish_id: dish_id)
-	    redirect_to dish_reviews_path({:dish => dish_id, :restaurant => restaurant_id})
+	    redirect_to dish_reviews_path(dish_id)
 	    flash[:notice] = "Review was successfully submitted."
 	end
 end

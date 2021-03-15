357a6665bca76bc593e853eeff07b889c7e6c568
Yanhao Li <hi@yanhaoli.com>
2021-03-15 17:45:25 -0400

    refactor: routes design

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
-  get "/restaurant_reviews", to: "restaurant_reviews#index"
-  get "/restaurant_reviews/new", to: "restaurant_reviews#new"
-  post "/restaurant_reviews", to: "restaurant_reviews#create"
-  get "/home", to: "home#index"
-  get "/dish_reviews", to: "dish_reviews#index"
-  get "dish_reviews/new", to: "dish_reviews#new"
-  post "/dish_reviews", to: "dish_reviews#create"
+  
+  # list of restaurants
+  get "/restaurants", to: "restaurants#index"
+
+  # details of restaurant, include dishes
+  get "/restaurants/:id", to: "restaurants#show"
+
+  # resturant reviews
+  get "/restaurants/:id/reviews" to "restaurant_reviews#index"
+  get "/restaurants/:id/reviews/new", to: "restaurant_reviews#new"
+  post "/restaurants/:id/reviews", to: "restaurant_reviews#create"
+
+  # dishes reviews
+  get "/dishes/:id/reviews", to: "dish_reviews#index"
+  get "/dishes/:id/reviews/new", to: "dish_reviews#new"
+  post "/dishes/:id/reviews", to: "dish_reviews#create"
 end

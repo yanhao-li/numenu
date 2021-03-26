7c8fb0ec23aabd1819dbac59e47168fc2de83d64
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2021-03-26 13:02:17 -0400

    almost have model rspec working

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -2,37 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
 
-	before(:all) do
-    	@dish = create(:dish)
-    	#let(:dish) { Dish.create dish_name: 'Dish with reviews' }
-      	let(:dish2) { Dish.create dish_name: 'Dish without reviews' }
 
-      	let(:review1) { DishReview.create rating: 1 }
-      	let(:review2) { DishReview.create ratin: 3 }
-      	let(:review2) { DishReview.create ratin: 5 }
-  	end
+  let(:dish) { Dish.create dish_name: 'Dish without reviews' }
+  let(:dish2) { Dish.create dish_name: 'Dish without reviews' }
+
+  let(:review1) { DishReview.create rating: 1 }
+  let(:review2) { DishReview.create ratin: 3 }
+  let(:review2) { DishReview.create ratin: 5 }
 
 	describe 'average_rating instance method' do
-    # 'build' creates but doesn't save object;  'create' creates AND saves it to the DB
-      
-
       
     it "should return the average of the review ratings if at least one review exists" do
+      expect(dish.average_rating(dish.id)).to eq 3
         
     end
 
     it "should return N/A if no reviews exist" do
-      expect(Movie.with_director('Martin Scorsese')).to eq ['Shutter Island', 'Hugo', 'The Wolf of Wall Street']
-      expect(Movie.with_director(movie3.director)).to eq ['Shutter Island', 'Hugo', 'The Wolf of Wall Street']
-      expect(Movie.with_director('Quentin Tarantino')).to eq ['Pulp Fiction', 'Once Upon a Time in Hollywood']
-      expect(Movie.with_director('Steven Spielberg')).to eq ['Jurassic Park']
+      expect(dish2.average_rating(dish.id)).to eq "N/A"
+      
     end
     
     it "should not return nil" do
-      expect(Movie.with_director('Martin Scorsese')).not_to eq ['Pulp Fiction', 'Once Upon a Time in Hollywood']
-      expect(Movie.with_director('Martin Scorsese')).not_to eq ['Shutter Island', 'Hugo', 'The Wolf of Wall Street', 'Once Upon a Time in Hollywood']
-      expect(Movie.with_director('Quentin Tarantino')).not_to eq ['Jurassic Park']
-      expect(Movie.with_director('Steven Spielberg')).not_to eq ['Pulp Fiction', 'Once Upon a Time in Hollywood']
+      expect(dish2.average_rating(dish2.id)).not_to eq nil
     end
       
   end

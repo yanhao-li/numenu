b8b59815e9f9596806a5db1f3794f707b7c1d9cc
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2021-03-25 23:44:58 -0400

    finished with average calculation in model and showing on the reviews pages

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,14 @@
 class Dish < ActiveRecord::Base
 	belongs_to :restaurant, class_name: 'Restaurant'
 	has_many :dish_reviews
+
+	def average_rating(dish_id)
+  		avg = Dish.joins(:dish_reviews).where(id: dish_id).average(:rating)
+  		if avg.nil? 
+  			"N/A"
+  		else
+  			avg
+  		end
+	end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,13 @@
 class Restaurant < ActiveRecord::Base
 	has_many :dishes
 	has_many :restaurant_reviews
+
+	def average_rating(restaurant_id)
+  		avg = Restaurant.joins(:restaurant_reviews).where(id: restaurant_id).average(:rating)
+  		if avg.nil? 
+  			"N/A"
+  		else
+  			avg
+  		end
+	end
 end

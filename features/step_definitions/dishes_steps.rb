cb77427edf8803ad56d429369ac7188b64891592
Matthew Otto <mattmotto18@gmail.com>
2021-03-28 18:32:23 -0500

    fixed errors in cucumber tests from duplicate entries existing (#70)

--- features/step_definitions/dishes_steps.rb
+++ features/step_definitions/dishes_steps.rb
@@ -1,5 +1,9 @@
 Given /the following dishes exist/ do |dishes_table|
   dishes_table.hashes.each do |dish|
+  	old = Dish.find_by id: dish["id"]
+  	if old != nil
+  		old.destroy
+  	end
     Dish.create dish
   end
 end
--- features/step_definitions/restaurant_steps.rb
+++ features/step_definitions/restaurant_steps.rb
@@ -1,5 +1,9 @@
 Given /the following restaurants exist/ do |restaurants_table|
   restaurants_table.hashes.each do |restaurant|
+  	old = Restaurant.find_by id: restaurant["id"]
+  	if old != nil
+  		old.destroy
+  	end
     Restaurant.create restaurant
   end
 end

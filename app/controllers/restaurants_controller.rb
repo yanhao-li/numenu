31c558bcb7c9d09ac936694763ec069c3b658970
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2021-03-29 09:10:22 -0400

    functionality works but need to integrate style

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,14 +2,20 @@ class RestaurantsController < ApplicationController
 	def index
 		if params[:restaurant_name]
 			searched_name = params[:restaurant_name].downcase
-			@restaurant = Restaurant.where("LOWER(name) LIKE ?", searched_name)
+			searched_name = '%' + searched_name + '%'  # add wildcards
+			@restaurants = Restaurant.where("LOWER(name) LIKE ?", searched_name)
 		else
 			@restaurants = Restaurant.all
 		end
-		#@restaurants = Restaurant.all
 	end
 
 	def show
 		@restaurant = Restaurant.find(params[:id])
+	end
+
+	private
+
+	def restaurant_params
+		params.require(:restaurant).permit(:id, :restaurant_name)
 	end
 end

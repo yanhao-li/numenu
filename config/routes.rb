4e6ed1bb48c2755ca6d0856fdf88dc9b181746e4
conniez <connieeez.97@gmail.com>
2021-03-29 09:47:19 +0900

    Made a homepage with map*untested due to rails error*

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  root 'home#index', as: "home"
+  get 'home/index'
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  resources :home, only: [:index]
   resources :restaurants, only: [:index, :show], shallow: true do
-    resources :reviews, :controller => "restaurant_reviews"
+  	resources :reviews, :controller => "restaurant_reviews"
     resources :dishes, only: [:index] do
-      resources :reviews, :controller => "dish_reviews"
+    	resources :reviews, :controller => "dish_reviews"
     end
   end
 end
+
+

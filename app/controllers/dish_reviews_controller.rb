d3e685059cdbf58c2c7ebf565ae89ff04f121089
Yanhao LI <yl4734@columbia.edu>
2021-04-18 16:56:32 -0400

    Fix/form post (#92)
    * fix: restaurant reviews post
    
    * fix: dish review post

--- app/controllers/dish_reviews_controller.rb
+++ app/controllers/dish_reviews_controller.rb
@@ -8,18 +8,15 @@ class DishReviewsController < ApplicationController
 	end
 
 	def create
-		dish_id = params[:dish_id]
-		inputs = params[:dish_review]
-		review = inputs[:review]
-		rating = inputs[:rating]
 		if session[:user_id] != nil
+				@dish_id = params[:dish_id]
 	    	@review = DishReview.create!(
-	    		review: review,
-	    		rating: rating,
-	    		dish_id: dish_id,
+	    		review: params[:review],
+	    		rating: params[:rating],
+	    		dish_id: @dish_id,
 	    		user_id: session[:user_id]
 	    	)
-	    	redirect_to dish_reviews_path(dish_id)
+	    	redirect_to dish_reviews_path(@dish_id)
 	    	flash[:notice] = "Review was successfully submitted."
 	    else
 	    	redirect_to dish_reviews_path(dish_id), alert: "Please login or sign up first"
--- app/controllers/restaurant_reviews_controller.rb
+++ app/controllers/restaurant_reviews_controller.rb
@@ -9,16 +9,19 @@ class RestaurantReviewsController < ApplicationController
 	end
 
 	def create
-		inputs = params[:restaurant_review]
-		restaurant_id = params[:restaurant_id]
 		if session[:user_id] != nil
+			@restaurant_id = params[:restaurant_id]
+			@review_content = params[:review]
+			@rating = params[:rating]
+
 			RestaurantReview.create!(
-				review: inputs[:review],
-				rating: inputs[:rating],
-				restaurant_id: restaurant_id,
+				review: @review_content,
+				rating: @rating,
+				restaurant_id: @restaurant_id,
 				user_id: session[:user_id]
 			)
-			redirect_to restaurant_reviews_path(restaurant_id)
+
+			redirect_to restaurant_reviews_path(@restaurant_id)
 			flash[:notice] = "Review was successfully submitted."
 		else
 			redirect_to restaurant_reviews_path(restaurant_id)
